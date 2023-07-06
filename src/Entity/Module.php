3b860f4bbf35f3a7ffd0997235737cf34a10fd5c
Badr El Rhazi <rzi.badr@gmail.com>
2023-07-06 12:25:07 +0100

    Module entity

---
+++ src/Entity/Module.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ModuleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ModuleRepository::class)]
+class Module
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 70)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToOne(inversedBy: 'modules')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?professor $professor = null;
+
+    #[ORM\ManyToOne(inversedBy: 'modules')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?filiere $filiere = null;
+
+    #[ORM\ManyToOne(inversedBy: 'modules')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?semester $semester = null;
+
+    #[ORM\OneToMany(mappedBy: 'module', targetEntity: Note::class)]
+    private Collection $notes;
+
+    public function __construct()
+    {
+        $this->notes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getProfessor(): ?professor
+    {
+        return $this->professor;
+    }
+
+    public function setProfessor(?professor $professor): static
+    {
+        $this->professor = $professor;
+
+        return $this;
+    }
+
+    public function getFiliere(): ?filiere
+    {
+        return $this->filiere;
+    }
+
+    public function setFiliere(?filiere $filiere): static
+    {
+        $this->filiere = $filiere;
+
+        return $this;
+    }
+
+    public function getSemester(): ?semester
+    {
+        return $this->semester;
+    }
+
+    public function setSemester(?semester $semester): static
+    {
+        $this->semester = $semester;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Note>
+     */
+    public function getNotes(): Collection
+    {
+        return $this->notes;
+    }
+
+    public function addNote(Note $note): static
+    {
+        if (!$this->notes->contains($note)) {
+            $this->notes->add($note);
+            $note->setModule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNote(Note $note): static
+    {
+        if ($this->notes->removeElement($note)) {
+            // set the owning side to null (unless already changed)
+            if ($note->getModule() === $this) {
+                $note->setModule(null);
+            }
+        }
+
+        return $this;
+    }
+}
